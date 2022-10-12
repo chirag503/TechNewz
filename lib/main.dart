141859d618c220cf48a40509169d5eaa51e6a86b
chirag <chiragsethi503@gmail.com>
2022-10-12 09:49:44 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:technewz/pages/home.dart';
+import 'package:technewz/utils/colors.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          brightness: Brightness.dark, primaryColor: AppColors.primary),
+      home: Home(),
+    );
+  }
+}
